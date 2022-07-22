67337a459378228aaf990da7cb3c340e2d1498d5
Vuong Hoang <vuonghoangbntt@gmail.com>
2022-07-22 10:45:20 +0700

    debug: fix attention in slot classifier

--- model/layer/SlotClassifier.py
+++ model/layer/SlotClassifier.py
@@ -7,19 +7,21 @@ from .attention import  Attention
 class SlotClassifier(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_slot_labels, num_intent_labels, use_attention=False, attention_input_dim= None, dropout_rate=0.0):
         super(SlotClassifier, self).__init__()
+        self.use_attention = use_attention
         if use_attention:
             self.attention = Attention(input_dim)
-        self.linear_intent_context = nn.Linear(num_intent_labels, input_dim)
+            self.linear_intent_context = nn.Linear(num_intent_labels, input_dim)
         self.dropout = nn.Dropout(dropout_rate)
         self.feedStart = FeedforwardLayer(d_in=input_dim, d_hid=hidden_dim)
         self.feedEnd = FeedforwardLayer(d_in=input_dim, d_hid=hidden_dim)
         self.biaffine = BiaffineLayer(inSize1=input_dim, inSize2=input_dim, classSize=num_slot_labels)
 
     def forward(self, x, intent_context, attention_mask):
-        intent_context = self.linear_intent_context(intent_context)
-        intent_context = torch.unsqueeze(intent_context, 1)  # 1: query length (each token)
-        output, weights = self.attention(x, intent_context, attention_mask)
-        x = self.dropout(output)
+        if self.use_attention:
+            intent_context = self.linear_intent_context(intent_context)
+            intent_context = torch.unsqueeze(intent_context, 1)  # 1: query length (each token)
+            output, weights = self.attention(x, intent_context, attention_mask)
+            x = self.dropout(output)
         start = self.feedStart(x)
         end = self.feedEnd(x)
         score = self.biaffine(start, end)
