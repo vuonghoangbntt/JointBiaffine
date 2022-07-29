3ee9ac70ae05b11b68ef9118360d6b8c576b3d09
Vuong Hoang <vuonghoangbntt@gmail.com>
2022-07-29 10:36:59 +0700

    debug: fix missing variable

--- model/jointBiaffine.py
+++ model/jointBiaffine.py
@@ -7,6 +7,7 @@ class JointBiaffine(nn.Module):
     def __init__(self, args):
         super(JointBiaffine, self).__init__()
         config = AutoConfig.from_pretrained(args.model_name_or_path)
+        self.args = args
         self.num_slot_labels = args.num_slot_labels
         self.num_intent_labels = args.num_intent_labels
         self.lstm_input_size = args.num_layer_bert * config.hidden_size
@@ -28,7 +29,7 @@ class JointBiaffine(nn.Module):
                           char_ids=char_ids)
         x, (hn, cn) = self.bilstm(x)
         intent_output = self.intent_classifier(hn.permute(1, 0, 2).reshape(x.shape[0], -1))
-        if not self.use_attention and self.attention_type == 'hard':
+        if not self.args.use_attention and self.args.attention_type == 'hard':
             slot_output = self.slot_classifier(x, hn.permute(1, 0, 2).reshape(x.shape[0], -1), attention_mask)
         else:
             slot_output = self.slot_classifier(x, intent_output, attention_mask)
